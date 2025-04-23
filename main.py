9ea9a91e4a5276f638216aff8c4ff3110f77ccb7
olechka4 <ol7278468@gmail.com>
2025-04-23 09:02:14 +0300

    .

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ b = int(input("Введите коэффициент b"))
 c = int(input("Введите коэффициент c"))
 
 disk = b**2 - 4*a*c
+if disk > 0:
+    x1 = (b**2 + disk**0.5) / 2*a
+    x2 = (b**2 - disk**0.5) / 2*a
+    print("x1 = ", x1, "x2 = ", x2)
